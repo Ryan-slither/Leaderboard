84f8a0293e92ee35bca994e15b6bf545ceac654d
Ryan-slither <ryanpfroug@gmail.com>
2026-02-12 18:52:07 -0500

    Cors Config

--- src/main/java/com/osc/leaderboard/repo/controller/RepoController.java
+++ src/main/java/com/osc/leaderboard/repo/controller/RepoController.java
@@ -3,16 +3,14 @@ package com.osc.leaderboard.repo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.osc.leaderboard.repo.service.RepoService;
-
 @RestController
 @RequestMapping("/repo")
 public class RepoController {
 
-    private final RepoService repoService;
+    // private final RepoService repoService;
 
-    public RepoController(RepoService repoService) {
-        this.repoService = repoService;
-    }
+    // public RepoController(RepoService repoService) {
+    // this.repoService = repoService;
+    // }
 
 }
